b7f9e60d87a46a6a4c16887c536cd567cfa02c4c
fuzzytake <giuseppe_vullo@yahoo.it>
2021-02-08 20:05:22 +0100

    created user postgres

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -14,8 +14,7 @@ class BookTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "bookshelf_test"
-        self.database_path = "postgres://{}/{}/{}/{}".format('postgres', 'postgres', 'localhost:5432',
-                                                             self.database_name)
+        self.database_path = "postgres://{}:{}@{}/{}".format('fuzzytake','postgres','localhost:5432',self.database_name)
         setup_db(self.app, self.database_path)
 
         self.new_book = {
--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ pip3 install flask_sqlalchemy
 pip3 install flask_cors
 pip3 install flask --upgrade
 pip3 uninstall flask-socketio -y
-service postgresql start
-su - postgres bash -c "psql < /home/workspace/backend/setup.sql"
-su - postgres bash -c "psql bookshelf < /home/workspace/backend/books.psql"
+brew services restart postgresql
+sudo su postgres bash -c "psql < /home/workspace/backend/setup.sql"
+sudo su postgres bash -c "psql bookshelf < /home/workspace/backend/books.psql"
